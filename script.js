79efb60b1ae2df33ee52284f99c98e6d9dd2b626
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2024-07-06 20:48:29 +0530

    feat - On button click Create new sketchpad with user given size

--- script.js
+++ script.js
@@ -4,11 +4,18 @@ const box = [];
 function sketchPad(boxPerRow){
     let totalBox = boxPerRow * boxPerRow;
     
+    // Formula to align a particular number of boxes in a Row
+    let width = `calc(100% * (1/${boxPerRow}))`; // 100% in formula refers to fixed width 960px [check CSS container]
+
+    // Creating boxes for the given grid
     for(let i=0; i<totalBox; i++){
         box[i] = document.createElement("div");
         box[i].className = "box";
+        box[i].style.setProperty('width', width);
         container.appendChild(box[i]);
     }
+
+    //Detecting selected boxes and changing color on them
     const grid = container.childNodes;
     grid.forEach((box)=>{
         box.addEventListener("mouseover", (event)=>{
@@ -17,16 +24,19 @@ function sketchPad(boxPerRow){
     });
 }
 
-
-
 function newSketchPad(){
-    const boxWidth = document.querySelector("div.box");
-    boxPerRow = prompt("ENTER SIZE FOR GRID: ");
-    boxWidth.style.width = `calc(100% * 1/${boxPerRow})`;
+    let boxPerRow = prompt("ENTER SIZE FOR GRID: ");
+    // Check for right value
+    if(boxPerRow >100){
+        boxPerRow = 100;
+    }else if(boxPerRow<=0){
+        boxPerRow = 1;
+    }
     removeAllElements();
     sketchPad(boxPerRow);
 }
 
+// Removing already existing elements from grid to create new
 function removeAllElements(){
     let noOfElements = box.length;
     for(let i=0; i< noOfElements; i++){
