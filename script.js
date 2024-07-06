64a06407c6170eb3712c1dad8f8733e4925e5301
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2024-07-06 20:11:58 +0530

    Feat - Button to remove elements and start new sketchpad

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 const container = document.querySelector("div.container");
-const box = []; 
+const box = [];
 
-for(let i=0; i<256; i++){
-    box[i] = document.createElement("div");
-    box[i].className = "box";
-    container.appendChild(box[i]);
+function sketchPad(boxPerRow){
+    let totalBox = boxPerRow * boxPerRow;
+    
+    for(let i=0; i<totalBox; i++){
+        box[i] = document.createElement("div");
+        box[i].className = "box";
+        container.appendChild(box[i]);
+    }
+    const grid = container.childNodes;
+    grid.forEach((box)=>{
+        box.addEventListener("mouseover", (event)=>{
+            event.target.className = "select";
+        })
+    });
 }
 
-const grid = container.childNodes;
 
-grid.forEach((box)=>{
-    box.addEventListener("mouseover", (event)=>{
-        event.target.className = "select";
-    })
-});
+
+function newSketchPad(){
+    const boxWidth = document.querySelector("div.box");
+    boxPerRow = prompt("ENTER SIZE FOR GRID: ");
+    boxWidth.style.width = `calc(100% * 1/${boxPerRow})`;
+    removeAllElements();
+    sketchPad(boxPerRow);
+}
+
+function removeAllElements(){
+    let noOfElements = box.length;
+    for(let i=0; i< noOfElements; i++){
+        container.removeChild(box[i]);
+    }
+}
+
+sketchPad(16);
